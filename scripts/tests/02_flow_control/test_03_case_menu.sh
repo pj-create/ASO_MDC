e0af80d5bb2818bea6a30105d6ebd3ef274183cc
Pablo <pjodmon0503@g.educaand.es>
2025-12-02 21:39:32 +0100

    añadido el test

---
+++ scripts/tests/02_flow_control/test_03_case_menu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source "/scripts/tests/base_test.sh"
+SCRIPT="/scripts/02_flow_control/03_case_menu.sh"
+
+test_case "fecha" \
+        "echo -ne \"1\n\" | $SCRIPT" \
+        "$(date)" \
+        0
+
+test_case "mostrarusuario" \
+        "echo -ne \"2\n\" | $SCRIPT" \
+        "$(whoami)" \
+        0
+
+test_case "mensaje_despedida" \
+        "echo -ne \"1\n\" | $SCRIPT" \
+        "Adiós" \
+        0
+
+summary || exit 1
